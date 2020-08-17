a620476ef1f9753ceb2026a3f980ae4846fc80d9
bjorn3 <bjorn3@users.noreply.github.com>
2020-08-17 12:22:34 +0200

    [DEBUG]

--- test.sh
+++ test.sh
@@ -31,8 +31,14 @@ fi
 echo "[AOT] mini_core_hello_world"
 $RUSTC example/mini_core_hello_world.rs --crate-name mini_core_hello_world --crate-type bin -g --target $TARGET_TRIPLE
 #dyldinfo -bind example/mini_core_hello_world
+objdump -t example/mini_core_hello_world || true
+objdump -weak-bind example/mini_core_hello_world || true
+objdump -indirect-symbols example/mini_core_hello_world || true
+objdump -r example/mini_core_hello_world || true
 lldb -O "set set target.disable-aslr false" -o "run" -o "bt" -o "dis" -- ./target/out/mini_core_hello_world abc bcd
 # (echo "break set -n main"; echo "run"; sleep 1; echo "si -c 10"; sleep 1; echo "frame variable") | lldb -- ./target/out/mini_core_hello_world abc bcd
+
+exit 1
 
 echo "[AOT] arbitrary_self_types_pointers_and_wrappers"
 $RUSTC example/arbitrary_self_types_pointers_and_wrappers.rs --crate-name arbitrary_self_types_pointers_and_wrappers --crate-type bin --target $TARGET_TRIPLE
